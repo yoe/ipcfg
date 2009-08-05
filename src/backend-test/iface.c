e1ba269d7e85dc48c6ff32d1e70113a417eaa9af
Wouter Verhelst <w@uter.be>
2009-08-05 17:54:17 +0200

    Stop pretending everything exists
    This way, we can test the "does not exist" case, too.

--- src/backend-test/iface.c
+++ src/backend-test/iface.c
@@ -1,9 +1,13 @@
 #include <ipcfg/backend/iface.h>
 
 #include <stdio.h>
+#include <string.h>
 
 /* Assume everything that's thrown at us exists */
 bool be_ifname_exists(char* name) {
 	printf("be_test: ifname_exists %s\n", name);
-	return true;
+	if(strncmp(name, "eth", 3)) {
+		return true;
+	}
+	return false;
 }

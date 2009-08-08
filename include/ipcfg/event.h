72fd6029d17ec623b869f8e52d0913ccf99f09a8
Wouter Verhelst <w@uter.be>
2009-08-08 10:43:25 +0200

    Copyright stuff
    - Credit beerware license origins
    - Distribute copyright to individual files.

--- include/ipcfg/event.h
+++ include/ipcfg/event.h
@@ -1,3 +1,13 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <wouter@debian.org> wrote this software. As long as you retain this
+ * notice you can do whatever you want with this stuff. If we meet some
+ * day, and you think this stuff is worth it, you can buy me a beer in
+ * return. Wouter Verhelst.
+ * ----------------------------------------------------------------------------
+ * (with thanks to phk@FreeBSD.org)
+ */
 #ifndef IPCFG_EVENT_H
 #define IPCFG_EVENT_H
 
--- src/ll.c
+++ src/ll.c
@@ -1,5 +1,14 @@
 /* Implementation of circularly-linked lists */
-
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <wouter@debian.org> wrote this software. As long as you retain this
+ * notice you can do whatever you want with this stuff. If we meet some
+ * day, and you think this stuff is worth it, you can buy me a beer in
+ * return. Wouter Verhelst.
+ * ----------------------------------------------------------------------------
+ * (with thanks to phk@FreeBSD.org)
+ */
 #include <ipcfg/ll.h>
 #include <stdlib.h>
 

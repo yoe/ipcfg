360ad76b8de197f0039792fed5b9329de570f3aa
Wouter Verhelst <w@uter.be>
2009-08-25 15:49:43 +0200

    Move to correct place
    Stuff that isn't gcc-specific doesn't need to be inside #ifdef GCC.

--- include/ipcfg/macros.h
+++ include/ipcfg/macros.h
@@ -11,10 +11,6 @@
 #ifndef IPCFG_MACROS_H
 #define IPCFG_MACROS_H
 
-#ifdef __GNUC__
-
-# define IPCFG_EXPECT_TRUE(cond) __builtin_expect((((cond)!=0)), 1)
-# define IPCFG_EXPECT_FALSE(cond) __builtin_expect((cond), 0)
 # define IPCFG_TODO fprintf(stderr, "E: %s implementation incomplete!\n", __func__); exit(EXIT_FAILURE);
 
 # ifdef DODBG
@@ -23,11 +19,15 @@
 #  define DEBUG(msg, args...)
 # endif // DODBG
 
+#ifdef __GNUC__
+
+# define IPCFG_EXPECT_TRUE(cond) __builtin_expect((((cond)!=0)), 1)
+# define IPCFG_EXPECT_FALSE(cond) __builtin_expect((cond), 0)
+
 #else // ! __GNUC__
 
 # define IPCFG_EXPECT_TRUE(cond) (cond)
 # define IPCFG_EXPECT_FALSE(cond) (cond)
-# define IPCFG_TODO fprintf(stderr, "E: a function was called which has not yet been implemented!\n"); exit(EXIT_FAILURE);
 
 #endif // __GNUC__
 

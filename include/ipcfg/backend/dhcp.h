4c9f62d2bd7ff9a36e513d441752dc1fb93d12ec
Wouter Verhelst <w@uter.be>
2009-09-14 07:14:39 +0200

    Update to new API
    This file existed from before there was an ipcfg_context structure; so
    update it to make functions accept that parameter, too.

--- include/ipcfg/backend/dhcp.h
+++ include/ipcfg/backend/dhcp.h
@@ -12,15 +12,16 @@
 #define IPCFG_BACKEND_DHCP_H
 
 #include <ipcfg/cnode.h>
+#include <ipcfg/context.h>
 #include <stdbool.h>
 
 /* Run the DHCP client, and set up the device with the resulting
  * configuration. Does a DHCP release on ifdown. */
-int be_do_dhcp(ipcfg_cnode*, ipcfg_action);
+int be_do_dhcp(ipcfg_cnode*, ipcfg_action, ipcfg_context* ctx);
 /* Test whether there is a DHCP server on the network by trying to
  * acquire a DHCP lease, but do not yet activate it. Does nothing on
  * ifdown. */
-int be_test_dhcp(ipcfg_cnode*, ipcfg_action);
+int be_test_dhcp(ipcfg_cnode*, ipcfg_action, ipcfg_context* ctx);
 /* Returns true if a DHCP client is running for the given interface.
  * Returns false if the interface is down, no such interface exists, or
  * no DHCP client is running for that interface. */

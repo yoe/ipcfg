3a870328007062e0991a4cb47fc84ea69c065a20
Wouter Verhelst <w@uter.be>
2009-09-30 18:40:17 +0200

    Implement indirect loading
    Allows to do stuff like
    
    iface "wlan0" {
    	test "wireless:essidlist" ("home", "work")
    	action "indirect_config" "wireless:essid"
    }
    
    config "home" {
    	# do stuff
    }
    
    config "work" {
    	# do other stuff
    }

--- src/core.c
+++ src/core.c
@@ -14,8 +14,11 @@
 #include <ipcfg/cnode.h>
 #include <ipcfg/context.h>
 #include <ipcfg/test.h>
+#include <ipcfg/ll.h>
+#include <ipcfg/config.h>
+#include <ipcfg/action.h>
 
-int test_network(ipcfg_cnode* node, ipcfg_action act, ipcfg_context* ctx) {
+static int test_network(ipcfg_cnode* node, ipcfg_action act, ipcfg_context* ctx) {
 	ipcfg_context_data* data = ipcfg_ctx_lookup_data(ctx, "core", "network");
 	if(data) {
 		char* havenetwork = (char*)data->data;
@@ -29,6 +32,19 @@ int test_network(ipcfg_cnode* node, ipcfg_action act, ipcfg_context* ctx) {
 	return 1;
 }
 
+static int load_config(ipcfg_cnode* node, ipcfg_action act, ipcfg_context* ctx) {
+	DLList* l = node->data;
+	char* name = l->data;
+	ipcfg_context_data* d = ipcfg_ctx_lookup_data(ctx, "core", name);
+	ipcfg_cnode* othernode = ipcfg_find_config("core", d->data);
+	if(othernode) {
+		return ipcfg_perform_confignode(othernode, act, ctx);
+	} else {
+		return 0;
+	}
+}
+
 void p_ipcfg_core_init(void) {
 	ipcfg_register_test("core", "network", test_network);
+	ipcfg_register_action("core", "indirect_config", load_config);
 }

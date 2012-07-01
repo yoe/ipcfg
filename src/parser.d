450a9018e0c24d9ddb68551bdc2bd62c4144414d
Wouter Verhelst <w@uter.be>
2012-07-01 21:17:49 +0200

    Commit current state
    The tokenizer is broken, since it can only figure out keywords, not
    identifiers -- which makes it fairly useless. I'll have to rip out most
    of it and start over, but commit this first so we can get back to it
    later, should that be necessary.

--- src/parser.d
+++ src/parser.d
@@ -4,15 +4,28 @@ import std.stdio;
 
 class TokenException : object.Exception {
 	private string partialtoken;
-	this(string pt, string file = __FILE__, uint line = __LINE__) {
+	private string msg;
+	this(string pt, string msg, string file = __FILE__, uint line = __LINE__) {
 		partialtoken = pt;
-		super("unknown token found.", file, line);
+		super(msg ~ " found.", file, line);
 	}
 	void prepend(string s) {
 		partialtoken = s ~ partialtoken;
 	}
 	override string toString() {
-		return "unknown token " ~ partialtoken ~ " found.";
+		return msg ~ partialtoken ~ " found.";
+	}
+}
+
+class UnknownTokenException : TokenException {
+	this(string pt, string msg = "unknown token", string file = __FILE__, uint line = __LINE__) {
+		super(pt, msg, file, line);
+	}
+}
+
+class DuplicateTokenException : TokenException {
+	this(string pt, string msg = "duplicate token", string file = __FILE__, uint line = __LINE__) {
+		super(pt, msg, file, line);
 	}
 }
 
@@ -26,15 +39,22 @@ class Tokenizer {
 		if(token.length > 1) {
 			Tokenizer* t = (token[0] in children);
 			if(t !is null) {
-				t.add_token(token[1..$], token_val);
+				try {
+					t.add_token(token[1..$], token_val);
+				} catch (DuplicateTokenException d) {
+					e.prepend(token[0..1]);
+				}
 			} else {
 				children[token[0]] = new Tokenizer(token[1..$], token_val);
 			}
 		} else {
+			if(tval) {
+				throw new DuplicateTokenException(s);
+			}
 			tval = token_val;
 		}
 	}
-	int get_tokenval(ref string stream) {
+	int get_tokenval(string stream = "") {
 		try {
 			if(tval) {
 				return tval;
@@ -42,11 +62,23 @@ class Tokenizer {
 			if(stream.length > 1) {
 				return children[stream[0]].get_tokenval(stream[1..$]);
 			}
-		} catch (TokenException t) {
+		} catch (UnknownTokenException t) {
 			t.prepend(stream[0..1]);
 			throw t;
 		}
-		throw new TokenException(stream[0..1], __FILE__, __LINE__);
+		throw new UnknownTokenException(stream[0..1], __FILE__, __LINE__);
+	}
+	Tokenizer get_child_token(char n) {
+		if(children[n]) {
+			return children[n];
+		}
+		throw new UnknownTokenException(n);
+	}
+	@property bool is_final() {
+		if(tval) {
+			return true;
+		}
+		return false;
 	}
 }
 
@@ -56,7 +88,29 @@ class Parser {
 		f.open(filename);
 	}
 	void parse() {
+		Tokenizer t = new Tokenizer("", 0)
+		t.add_token("auto", 1024);
+		t.add_token("iface", 1025);
+		t.add_token("	", 1026);
+		t.add_token("inet", 1027);
+		t.add_token("dhcp", 1028);
+		Tokenizer ct = t;
 		foreach(ubyte[] buf; f.byChunk(4096)) {
+			foreach(ubyte b; buf) {
+				try {
+					ct = ct.get_child_token(b);
+					if(ct.is_final) {
+						std.writeln(ct.get_tokenval());
+						ct = t;
+					}
+				} catch (TokenException e) {
+					std.writeln("E: " ~ e.toString());
+				}
+			}
 		}
 	}
+}
+
+unittest {
+	Parser p("");
 }
